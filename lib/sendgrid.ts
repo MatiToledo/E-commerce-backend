b216406eddcbe0eb00f7b5349f643412448b8b63
Mati Toledo <toledo.matias@icloud.com>
2022-06-06 12:49:52 -0300

    sgmail

--- lib/sendgrid.ts
+++ lib/sendgrid.ts
@@ -2,12 +2,16 @@ import sgMail, { MailDataRequired } from "@sendgrid/mail";
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 export async function sendgridEmail(msg: MailDataRequired) {
-  sgMail
-    .send(msg)
-    .then(() => {
-      return true;
-    })
-    .catch((error) => {
-      return error;
-    });
+  try {
+    sgMail
+      .send(msg)
+      .then(() => {
+        return true;
+      })
+      .catch((error) => {
+        return error;
+      });
+  } catch (error) {
+    return error;
+  }
 }
